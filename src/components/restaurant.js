2f7cdd082a26cd7cf0b7ea6b7b0f02790b8f87b7
Evgeny Strjakhilev <evg.strjahilev@gmail.com>
2020-09-03 17:18:06 +0300

    HT1-striakhilev modified code which was commented

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -6,21 +6,22 @@ import Rate from './rate';
 function Restaurant({ restaurant }) {
   const { reviews, name, menu } = restaurant;
   // const [sumRate, setSumRate] = useState(0);
-
+  //   const average2 = useMemo(
+  //       () => reviews.map(review => {
+  //           setSumRate(sumRate => sumRate + review.rating)
+  //           debugger
+  //       }),
+  //       [sumRate, reviews]
+  //   );
   let average = 0;
-
+  debugger;
   return (
     <div>
       <h1>{name}</h1>
       {reviews.map((review) => {
         average = (average + review.rating) / reviews.length;
       })}
-      {/*{*/}
-      {/*    reviews.map((review) => {*/}
-      {/*        setSumRate(sumRate => sumRate + review.rating)*/}
-      {/*        return <Rate rate={sumRate/reviews.length}/>*/}
-      {/*    }) */}
-      {/*}*/}
+      {/*<Rate rate={average2}/>*/}
 
       <Rate rate={average} />
       <Menu menu={menu} />
