238e9f39451fbf80f369370efc5276d1927045b5
Andrii Koretskyi <andriiko@wix.com>
2020-09-15 19:11:44 +0300

    add Loader

---
+++ src/components/loader/loader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './loader.module.css';
+
+function Loader() {
+  return (
+    <div className={styles.loader}>
+      <div className={styles.bounce1} />
+      <div className={styles.bounce2} />
+    </div>
+  );
+}
+
+Loader.propTypes = {};
+
+export default Loader;
