5b06d8eec45aabbc8b695dbb7ea12e04ceafc9fa
Andrii Koretskyi <andriiko@wix.com>
2020-09-25 14:01:39 +0300

    add HT7.1

--- src/pages/restaurants-page.js
+++ src/pages/restaurants-page.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import Restaurants from '../components/restaurants';
 import Loader from '../components/loader';
 import {
@@ -26,12 +26,7 @@ function RestaurantsPage({
   if (loading || !loaded) return <Loader />;
 
   if (match.isExact) {
-    return (
-      <>
-        <Restaurants match={match} history={history} />
-        <h2 style={{ textAlign: 'center' }}>Select restaurant</h2>
-      </>
-    );
+    return <Redirect to={`/restaurants/${restaurants[0].id}`} />;
   }
 
   return <Route path="/restaurants/:restId" component={Restaurants} />;
